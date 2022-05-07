6b0c092fe0f8543b60b63627a5384126cbf74971
aniket777984 <aniket777984@gmail.com>
2022-05-07 22:01:34 +0530

    Final commit

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ import {userRouter} from "./routes/User.js"
 
 app.use("/api/v1" , userRouter);
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(_dirname, "../frontend/build")));
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+    res.sendFile(path.resolve(_dirname, "../frontend/build/index.html"));
 });
