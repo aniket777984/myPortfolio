8b148ccad8b86ed7cba065e757332074803a169f
aniket777984 <98917933+aniket777984@users.noreply.github.com>
2023-06-20 01:33:35 +0530

    Deployed

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
     require("dotenv").config({ path: "backend/config/config.env" });
 }
 
-app.use(express.json({limit:"50mb"}));
+app.use(express.json());
 app.use(express.urlencoded({extended:true,limit:"50mb"}));
 app.use(cookieParser());
 
